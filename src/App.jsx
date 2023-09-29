689eb7741a70dc40c1564d8c838813a59e518bb8
rafidardanu <rescuephones2016@gmail.com>
2023-09-29 17:19:37 +0700

    merge app and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Router from './router/Router'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import CreateProduct from "./pages/CreateProduct";
+import ProductDetail from "./pages/ProductDetail";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "/CProduct",
+    element: <CreateProduct />,
+  },
+  {
+    path: "/CProduct/:id",
+    element: <ProductDetail />,
+  },
+]);
 
 function App() {
   return (
-    <Router />
+    <RouterProvider router={router}>
+      <Route />
+    </RouterProvider>
   );
 }
 
-export default App
-
-// // eslint-disable-next-line no-unused-vars
-// import React from "react";
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import { BrowserRouter, Route, Switch } from "react-router";
-// import CreateProduct from './pages/CreateProduct.jsx';
-// import LandingPage from './pages/LandingPage.jsx';
-
-// function App(){
-//     return(
-//         <BrowserRouter>
-//             <Switch>
-//                 <Route path="/" exact component={LandingPage} />
-//                 <Route path="/CProduct" exact component={CreateProduct} />
-//             </Switch>
-//         </BrowserRouter>
-//     );
-// }
-// export default App;
+export default App;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,27 +1,27 @@
-import ProductDetail from '../pages/ProductDetail'
-import CreateProduct from '../pages/CreateProduct'
-import LandingPage from '../pages/landingPage'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import ProductDetail from '../pages/ProductDetail'
+// import CreateProduct from '../pages/CreateProduct'
+// import LandingPage from '../pages/landingPage'
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-const router = createBrowserRouter([
-{
-    path: '/',
-    element: <LandingPage />
-},
-{
-    path: '/CProduct',
-    element: <CreateProduct />
-},
-{
-    path: '/CProduct/:id',
-    element: <ProductDetail />
-},
-])
+// const router = createBrowserRouter([
+// {
+//     path: '/',
+//     element: <LandingPage />
+// },
+// {
+//     path: '/CProduct',
+//     element: <CreateProduct />
+// },
+// {
+//     path: '/CProduct/:id',
+//     element: <ProductDetail />
+// },
+// ])
 
-function Router() {
-    return (
-        <RouterProvider router={router}/>
-    )
-}
+// function Router() {
+//     return (
+//         <RouterProvider router={router}/>
+//     )
+// }
 
-export default Router
+// export default Router
